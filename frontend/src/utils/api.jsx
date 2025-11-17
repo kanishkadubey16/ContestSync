e220cf30d643ca103871d0188577f6cb2e461728
kanishkadubey16 <kanishkadubey16@gmail.com>
2025-11-17 17:51:45 +0530

    Fixed axios error

--- frontend/src/utils/api.jsx
+++ frontend/src/utils/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://contestsync-1.onrender.com",
+  baseURL: "https://contestsync-1.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
